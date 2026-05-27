cbb55037620259734b211c9315197d82b750c320
Aryan Patel <ap9384907>
2026-05-27 17:06:50 +0530

    Creating custom Modules

---
+++ .vscode/tut68/index.js
@@ -0,0 +1,12 @@
+// const average = require("./mod");
+// console.log(average([3, 4]));
+const mod = require("./mod");
+// mod ={
+//     avg: average,
+//     name: "Aryan",
+//     repo: "Web Dev"
+// }
+// console.log(mod.avg([3, 4]));
+console.log(mod.name);
+console.log("This is index.js");
+
